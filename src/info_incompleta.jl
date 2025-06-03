065053a5e2cddece7417d5b74180f3f3fc323161
edtrelo <edtrelo18@ciencias.unam.mx>
2025-06-02 19:17:35 -0600

    Añade análisis de residuales a info incompleta

--- src/info_incompleta.jl
+++ src/info_incompleta.jl
@@ -7,41 +7,17 @@ using Optim
 using FiniteDiff 
 using Bootstrap
 using Statistics
-Random.seed!(123)
-
-# Caso 1: Usando datos sintéticos
-
-# Parámetros para la simulaciñón
-M = 10000
-dt = 1/M
-N = 763
-β = 0.0013925
-γ = 0.476
-σ = 0.0005
-p = (β, γ, σ, N)
-I₀ = 10
-
-# Queremos ver cómo mejora la estimación según usemos más datos
-nDias = [7, 14, 28, 50]
-dW = rand(Normal(0, 1), trunc(Int, nDias[end]/dt))
-X = solveSIS(p, nDias[end], I₀, dW; dt = dt)
-ts = collect(0:dt:nDias[end])
-# Trayectoria completa
-tray = [X ts]
-
-obs = tray[(tray[1:end, 2].%1 .== 0), 1]
-obs_ts = tray[(tray[1:end, 2].%1 .== 0), 2]
-
-
+using StatsPlots
+Random.seed!(1)
 
 N = 200
 I₀ = 0.0912*N*10^(-3)
 obs = [0.1397, 0.1966, 0.2530, 0.3476, 0.4575, 0.6142, 0.7986, 0.8176, 0.6257, 0.8376, 0.3979]*10^(-3)
 obs = obs.*N
 obs_ts = collect(1:length(obs))
-γ = 31.39
+# 3 a 6 semana
+γ = 52/4.5
 
-γ = 1/2
 sss = SIS_sigma_by_qv(obs, obs_ts)
 b = 0
 for i in 1:(length(obs)-1)
@@ -55,43 +31,21 @@ h = 1/M
 
 x0 = [b, γ, sss]
 
-
-α = 10^(-4) 
-xnew = ∇log_likelihood(x0, DZ, M, S, h, obs, N, obs_ts)
-
-# H*∇log_likelihood(x0, DZ, M, S, h, obs, N, obs_ts)
-u = optimize(f, [α])
-u.minimizer
-# inv(FiniteDiff.finite_difference_hessian(F, x0))
-# FiniteDiff.finite_difference_jacobian(∇F, x0)
-
-lower = [0, 365/28, 0]
-upper = [Inf, 365/1, Inf]
+lower = [0, 52/6, 0]
+upper = [Inf, 52/3, Inf]
 
 DZ = rand(Normal(0, 1), (S, M+1))
 oldlike = F(x0)
-#sol = optimize(x -> -F(x), lower, upper, x0, Fminbox(BFGS()); inplace = false)
-sol = optimize(x -> -F(x), x0, BFGS())
+sol = optimize(x -> -F(x), lower, upper, x0, Fminbox(BFGS()); inplace = false)
+#sol = optimize(x -> -F(x), x0, BFGS())
 x0 = sol.minimizer
 
 
-# x0 = [b, γ, sss]
-# oldlike = F(x0)
-# for i in 1:1000
-#     #xnew = ∇log_likelihood(x0, DZ, M, S, h, obs, N, obs_ts
-#     try 
-#         xnew = ∇F(x0)
-#         H = FiniteDiff.finite_difference_hessian(x-> -F(x), x0)
-#     catch 
-#         α = α/10 
-
-#     x0 += α*inv(H)*xnew
-# end
 
 F(x0)>oldlike
 x0
 
-T = 50
+T = obs_ts[end]
 p = plot(obs_ts, obs)
 ts = collect(0:0.001:T)
 sim = zeros(100, length(ts))
@@ -110,9 +64,6 @@ for i in 1:length(ts)
     CIL[2, i] = mean(datai)
     CIL[1, i] = quantile(datai, 0.05)
     CIL[3, i] = quantile(datai, 0.95)
-    # bs = bootstrap(mean, datai, BasicSampling(100))
-    # cil, = Bootstrap.confint(bs, BasicConfInt(0.95))
-    # CIL[1:end, i] .= cil
 end
 
 CIL
@@ -127,3 +78,25 @@ plot!(ts, m, ribbon = (m.-low, upper.-m))
 plot!(ts, m)
 plot!(ts, low)
 plot!(ts, upper)
+
+
+# Validación del modelo :)
+β₀, γ₀, σ₀ = x0
+func_h(x) = (1/(N*σ₀))*log(x/(N-x))
+inv_h(x) = (N*exp(σ₀*N*x))/(1+exp(σ₀*N*x))
+
+func_b(x) = β₀*x*(N-x)-γ₀*x
+func_s(x) = σ₀*x*(N-x)
+func_ds(x) = σ₀*(N-2x)
+
+func_μ(x) = func_b(inv_h(x))/func_s(inv_h(x)) - (1/2)*func_ds(inv_h(x))
+
+n = length(obs)
+residuals = zeros(n-1)
+for i in 2:n
+    residuals[i-1] = obs[i] - obs[i-1] - func_μ(obs[i-1])
+end
+
+plot(qqnorm(x), xlabel = "Datos", ylabel = "Normales", 
+title = "Comparación de residuales")
+
